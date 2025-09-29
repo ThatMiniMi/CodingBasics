c7a00e3536e910b94c845f2f5631b808292cb0cc
My <myglimsjo@gmail.com>
2025-09-29 08:22:17 +0200

    Added folders in Backend and Frontend

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f22b438751ab14d5e962018596a95f59a6823143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3cce49d9e2ce01e3d6dd00af631e6f20122ebe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
